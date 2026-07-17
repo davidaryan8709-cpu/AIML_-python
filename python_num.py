b825a864d03f889469fc64d4a30f499980cde684
Raushan <davidaryan8709@gmail.com>
2026-07-17 10:57:29 +0530

    added python_num.py

--- python_num.py
+++ python_num.py
@@ -1,6 +1,19 @@
-import numpy as np
-def square (number):
-    return number * number
-nums=[2,3,4,5,6]
-square_Numbers=list(map(square,nums))
-print(square_Numbers)
+# import numpy as np
+# def square (number):
+#     return number * number
+# nums=[2,3,4,5,6]
+# square_Numbers=list(map(square,nums))
+# print(square_Numbers)
+
+# def lamba(num1):
+#     list(map(lambda num:num*num,num1))
+
+# num1=[1,2,3,4]
+# print(lamba(num1))
+
+from functools import reduce 
+def sum_all(a,b):
+    return a+b 
+nums = [1,2,3,4]
+sum = reduce(sum_all,nums)
+print(sum)
