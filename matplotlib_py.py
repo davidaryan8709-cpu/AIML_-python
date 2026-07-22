c32ba07026e4ac7a27c28148ca7e7d9497d39c68
Raushan <davidaryan8709@gmail.com>
2026-07-22 13:03:07 +0530

    updated

--- matplotlib_py.py
+++ matplotlib_py.py
@@ -2,11 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 iris = pd.read_csv('iris.csv', names=['sepal length','sepal width','petal length','petal-width','class'])
 print(iris.head())
-fig, ax = plt.subplots()
-  
-
-ax.scatter(iris['sepal length'], iris['sepal width'])  
-ax.set_title('Iris Dataset') 
-ax.set_xlabel('sepal_length') 
-ax.set_ylabel('sepal_width')
+colors = {'Iris-setosa':'r','Iris-versicolor':'g', 'Iris-virginica':'b'}
+# create a figure and axis 
+fig, ax = plt.subplots() # plot each data-point for
+for i in range(len(iris['sepal length'])):
+ ax.scatter(iris['sepal length'][i],iris['sepal width'][i],color=colors[iris['class'][i]])
+# set a title and labels 
+ax.set_title('IrisDataset') 
+ax.set_xlabel('sepal_length')
+ax.set_ylabel('sepal_width') 
 plt.show()
